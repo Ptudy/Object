412af95ca0f7df0fb3b9368e216d130500f07d71
kimchijinju <hanbimkim@gmail.com>
2022-04-10 03:45:56 +0900

    DiscountCondition이 변경이 취약하다.

---
+++ kimchijinju/object/src/chap05/Screening.java
@@ -0,0 +1,27 @@
+package chap05;
+
+
+import java.time.LocalDateTime;
+
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money caculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public LocalDateTime getWhenScreened() {
+        return whenScreened;
+    }
+
+}
